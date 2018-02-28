98b32d71faab2560210cbb537155ec7482fa05b6
xiong.zeng <15150423473@163.com>
2018-02-28 10:58:59 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 90;
+        int sum = 0;
+        for (int k = 0; k < 10; k++) {
+            sum = +i;
+            sum = +j;
+
+        }
+        System.out.println("sum=" + sum);
+
+
+    }
+}
